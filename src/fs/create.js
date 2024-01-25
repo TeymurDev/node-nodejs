a477f0aed640902d19cb906c0ab24f5f21800b53
TeymurDev <teymur.ff@gmail.com>
2024-01-25 22:19:40 +0400

    feat: implement create js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const directoryPath = 'src/fs/files';
+  const fileName = 'fresh.txt';
+  const filePath = path.join(directoryPath, fileName);
+
+  if (fs.existsSync(filePath)) {
+    console.error('FS operation failed');
+  }
+
+  fs.writeFile(filePath, 'I am fresh and young', (err) => {
+    if (err) {
+      console.error('error creating file', err);
+    }
+  });
 };
 
 await create();

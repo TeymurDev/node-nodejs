e1cac2c6858aca69641db021a8b47dfb84fbd0f5
TeymurDev <teymur.ff@gmail.com>
2024-01-26 21:36:22 +0400

    feat: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+
 const remove = async () => {
-    // Write your code here 
+  const directoryPath = 'src/fs/files';
+
+  fs.unlink(`${directoryPath}/fileToRemove.txt`, (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      console.log('file successfully deleted');
+    }
+  });
 };
 
 await remove();

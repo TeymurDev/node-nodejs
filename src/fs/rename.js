f72a4a35b6aefd85427f37d03ca678aafa617e5c
TeymurDev <teymur.ff@gmail.com>
2024-01-27 22:10:38 +0400

    feat: implement rename js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+  const directoryPath = 'src/fs/files';
+
+  if (fs.existsSync(`${directoryPath}/properFilename.md`)) {
+    throw new Error('FS operation failed');
+  }
+
+  fs.rename(
+    `${directoryPath}/wrongFilename.txt`,
+    `${directoryPath}/properFilename.md`,
+    (err) => {
+      if (err) {
+        throw new Error('FS operation failed');
+      }
+      console.log('file successfully renamed');
+    },
+  );
 };
 
 await rename();
